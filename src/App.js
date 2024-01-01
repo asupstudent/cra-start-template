26206887432d84069dfaf23e7c7251fdb65c5d3b
asupstudent <asupstudent@yandex.ru>
2024-01-01 22:33:03 +0300

    Настраивает стартовый шаблон

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Настроил деплой.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Котокафе
-        </a>
+        <p>Шаблон React приложения.</p>
       </header>
     </div>
   );
